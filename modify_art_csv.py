7e7e0b7b4f80af29af1c578cffbf4201ad198198
Zoe Sheill <zsheill7@gmail.com>
2022-04-17 23:59:33 -0400

    modified Python file to do programmatic generation

--- modify_art_csv.py
+++ modify_art_csv.py
@@ -1,13 +1,21 @@
-#from tempfile import NamedTemporaryFile
-import shutil
 import csv
+import os
 
-filename_input = 'pandora_filepaths_no_commas.csv'
+filename_input = 'pandora_filepaths_input.csv'
 filename_output = 'pandora_filepath_output.csv'
-filename_remove_commas = 'pandora_commas_removed.csv'
-#tempfile = NamedTemporaryFile(mode='w', delete=False)
 
-# fields = ['ID', 'Name', 'Course', 'Year']
+path = "/Users/zoe/Documents/classes-sp22/9.60/Pandora_V1"
+
+with open(filename_input, 'w') as f:
+    writer = csv.writer(f)
+    for (root, dirs, file) in os.walk(path):
+        for f in file:
+            if '.jpg' in f:
+                filepath = root + os.sep + f
+                print("filepath")
+                print(filepath)
+                last_part = filepath.split("Pandora_V1/")[1]
+                writer.writerow([last_part])
 
 with open(filename_output, 'w') as csvfile2:
     with open(filename_input, 'r') as csvfile:
@@ -15,34 +23,17 @@ with open(filename_output, 'w') as csvfile2:
         writer = csv.writer(csvfile2)
         for row in reader:
             prev_row = row
-            prefix = "s3://artstylequalificationsurveyallperiods/Pandora_V1-20220406T013528Z-001/Pandora_V1"
-            print("row")
-            print(row)
-            row = [elem.strip(".") for elem in row]
-            row = [prefix + elem for elem in row]
+            prefix = "https://artstylequalificationsurveyallperiods.s3.amazonaws.com/Pandora_V1-20220406T013528Z-001/Pandora_V1/"
 
-            writer.writerow(row)
+            # The row should just have one element, the image link
+            image_link = row[0]
+            
+            # Add image link prefix to image link
+            image_link = prefix + image_link
 
-with open(filename_remove_commas, 'w') as csvfile2:
-    with open(filename_output, 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        writer = csv.writer(csvfile2)
-        for row in reader:
-            #prev_row = row
-            #prefix = "s3://artstylequalificationsurveyallperiods/Pandora_V1-20220406T013528Z-001/Pandora_V1"
+            row = [image_link]
 
-            row = [elem.replace(',', '\n') for elem in row]
-            #row = [prefix + elem for elem in row]
-
-            writer.writerow(row)
-
-# with open('path_to_csv_file', 'r') as csv_file:
-#     data = list(csv.reader(csv_file, delimiter=';'))
-
-# data = [(int(raw_row[0]), float(raw_row[1].replace(',', '.'))) for row in data]
-
-# with open('path_to_csv_file', 'w') as csv_file:
-#     writer = csv.writer(csv_file, delimiter=';')
-#     writer.writerows(data)
-
-#shutil.move(tempfile.name, filename2)
+            # Image titles with commas doesn't work with CSVs
+            # There is also a weird A-like character that doesn't work
+            if "," not in image_link and "tienne Istv" not in image_link:
+                writer.writerow(row)
